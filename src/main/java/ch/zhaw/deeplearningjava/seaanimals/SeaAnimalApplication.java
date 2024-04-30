5b9f14e057398a476ff43e5638393b329273531b
athika98 <athika@abira.ch>
2024-04-30 08:22:59 +0200

    first commit

---
+++ src/main/java/ch/zhaw/deeplearningjava/seaanimals/SeaAnimalApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.deeplearningjava.seaanimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaAnimalApplication.class, args);
+	}
+
+}
